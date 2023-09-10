373a06a2c316f2870b7ca21b9aa8800f8746505b
tusar877 <130102846+tusar877@users.noreply.github.com>
2023-09-10 08:42:59 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,17 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  useEffect(() => {
+  const openNewTab = () => {
     const url = 'https://warriorplus.com/o2/a/kc2ykv/0';
-    const numberOfTabsToOpen = 10;
+    window.open(url, '_blank');
+  };
 
-    for (let i = 0; i < numberOfTabsToOpen; i++) {
-      const newTab = window.open(url, '_blank');
-      if (newTab) {
-        newTab.focus();
-      }
-    }
-  }, []);
-
-  return null;
+  return (
+    <div>
+      <button onClick={openNewTab}>Open New Tab</button>
+    </div>
+  );
 };
 
 export default Home;
-
